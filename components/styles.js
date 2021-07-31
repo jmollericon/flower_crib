46bf923475c57c6fb1b716f6b82988bf50cb3d12
Jorge Mollericon <jlmollericon@gmail.com>
2021-07-31 14:22:03 -0400

    implements text input in Login screen

--- components/styles.js
+++ components/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
 import Constans from 'expo-constants'
 
 const StatusBarHeight = Constans.statusBarHeight
@@ -38,4 +38,63 @@ export const PageTitle = styled.Text`
   font-weight: bold;
   color: ${brand};
   padding: 10px;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: 18px;
+  margin-bottom: 20px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: ${tertiary};
+`;
+
+export const StyledFormArea = styled.View`
+  width: 90%;
+`;
+
+export const StyledTextInput = styled.TextInput`
+  background-color: ${secondary};
+  padding: 15px;
+  padding-left: 55px;
+  padding-right: 55px;
+  border-radius: 5px;
+  font-size: 16px;
+  height: 60px;
+  margin-vertical: 3px;
+  margin-bottom: 10px;
+  color: ${tertiary};
+`
+
+export const StyledInputLabel = styled.Text`
+  color: ${tertiary};
+  font-size: 13px;
+  text-align: left;
+`;
+
+export const LeftIcon = styled.View`
+  left: 15px;
+  top: 38px;
+  position: absolute;
+  z-index: 1;
+`;
+
+export const RightIcon = styled.TouchableOpacity`
+  left: 15px;
+  top: 38px;
+  position: absolute;
+  z-index: 1;
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+  padding: 15px;
+  background-color: ${brand};
+  justify-content: center;
+  border-radius: 5px;
+  margin-vertical: 5px;
+  height: 60px;
+`;
+
+export const ButtonText = styled.Text`
+  color: ${primary};
+  font-size: 16px;
 `;
--- screens/Login.js
+++ screens/Login.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 
-import {
-  StyledContainer,
-  InnerContainer,
-  PageLogo,
-  PageTitle
-} from '../components/styles'
+// formik
+import { Formik } from 'formik'
+
+// icons
+import { Octicons } from '@expo/vector-icons'
+
+import { StyledContainer, InnerContainer, PageLogo, PageTitle, SubTitle, StyledFormArea, LeftIcon, StyledInputLabel, StyledTextInput, RightIcon, Colors } from '../components/styles'
+import { View } from 'react-native'
+
+// Colors
+
+const { brand, darkLight } = Colors;
+
 
 const Login = () => {
   return (
@@ -15,8 +22,53 @@ const Login = () => {
       <InnerContainer>
         <PageLogo resizeMode="cover" source={require('./../assets/img/expo-bg1.png')} />
         <PageTitle>Flower Crib</PageTitle>
+        <SubTitle>Account Login</SubTitle>
+
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={(values) => {
+            console.log(values)
+          }}
+        >{({handleChange, handleBlur, handleSubmit, values}) => (
+          <StyledFormArea>
+            <MyTextInput
+              label='Email Address'
+              icon='mail'
+              placeholder='example@mail.com'
+              placeholderTextColor={darkLight}
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              value={values.email}
+              keyboardType='email-address'
+            />
+            <MyTextInput
+              label='Password'
+              icon='lock'
+              placeholder='* * * * * * * *'
+              placeholderTextColor={darkLight}
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+              value={values.password}
+              secureTextEntry={true}
+            />
+          </StyledFormArea>
+        )}
+
+        </Formik>
       </InnerContainer>
     </StyledContainer>
+  )
+}
+
+const MyTextInput = ({label, icon, ...props}) => {
+  return (
+    <View>
+      <LeftIcon>
+        <Octicons name={icon} size={30} color={brand} />
+      </LeftIcon>
+      <StyledInputLabel>{label}</StyledInputLabel>
+      <StyledTextInput {...props} />
+    </View>
   )
 }
 
